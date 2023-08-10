5030561883a37a269a88ef08225cd1e53757f77d
chillionaire128 <33501553+chillionaire128@users.noreply.github.com>
2023-08-10 14:02:35 -0700

    Main game instance work
    Added function to scan character directory on game start

--- Source/Modular2dFighter/MainGameInstance.cpp
+++ Source/Modular2dFighter/MainGameInstance.cpp
@@ -3,3 +3,34 @@
 
 #include "MainGameInstance.h"
 
+void UMainGameInstance::Init()
+{
+    Super::Init();
+    ScanCharactersDirectory();
+}
+
+void UMainGameInstance::ScanCharactersDirectory()
+{
+    IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+
+    // Assuming characters are stored in a "Characters" directory under the Content directory
+    FString CharacterFolderPath = FPaths::ProjectContentDir() + TEXT("Characters/");
+
+    // Array to hold found directories (character folders)
+    TArray<FString> CharacterDirectories;
+
+    // Find subdirectories (individual character folders)
+    PlatformFile.FindFiles(CharacterDirectories, *CharacterFolderPath, nullptr);
+
+    for (FString CharacterDir : CharacterDirectories)
+    {
+        FString SettingsFilePath = CharacterDir + TEXT("/Settings/settings.json");
+
+        // Check if the settings file exists
+        if (PlatformFile.FileExists(*SettingsFilePath))
+        {
+            // Print the path to the settings file for the character (or log it)
+            UE_LOG(LogTemp, Warning, TEXT("Found character settings: %s"), *SettingsFilePath);
+        }
+    }
+}
--- Source/Modular2dFighter/MainGameInstance.h
+++ Source/Modular2dFighter/MainGameInstance.h
@@ -14,4 +14,10 @@ class MODULAR2DFIGHTER_API UMainGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+	// Override the Init function from UGameInstance
+	virtual void Init() override;
+
+private:
+	void ScanCharactersDirectory();
 };
