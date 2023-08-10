f555d82f96546a40857923cde5c0fc075364a557
chillionaire128 <33501553+chillionaire128@users.noreply.github.com>
2023-08-10 15:07:33 -0700

    More Maingameinstance
    more work on main game instance scanning character files

--- Source/Modular2dFighter/MainGameInstance.cpp
+++ Source/Modular2dFighter/MainGameInstance.cpp
@@ -6,6 +6,7 @@
 void UMainGameInstance::Init()
 {
     Super::Init();
+    GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Executing ScanCharactersDirectory..."));
     ScanCharactersDirectory();
 }
 
@@ -16,21 +17,43 @@ void UMainGameInstance::ScanCharactersDirectory()
     // Assuming characters are stored in a "Characters" directory under the Content directory
     FString CharacterFolderPath = FPaths::ProjectContentDir() + TEXT("Characters/");
 
-    // Array to hold found directories (character folders)
+    // Print debug message indicating function execution
+    UE_LOG(LogTemp, Warning, TEXT("Executing ScanCharactersDirectory..."));
+
+    // Array to hold found directory entries (both files and directories)
     TArray<FString> CharacterDirectories;
 
-    // Find subdirectories (individual character folders)
-    PlatformFile.FindFiles(CharacterDirectories, *CharacterFolderPath, nullptr);
-
-    for (FString CharacterDir : CharacterDirectories)
-    {
-        FString SettingsFilePath = CharacterDir + TEXT("/Settings/settings.json");
-
-        // Check if the settings file exists
-        if (PlatformFile.FileExists(*SettingsFilePath))
+    // Using IterateDirectory to get directories
+    bool bFoundDirs = PlatformFile.IterateDirectory(*CharacterFolderPath, [&CharacterDirectories](const TCHAR* FilenameOrDirectory, bool bIsDirectory) -> bool
         {
-            // Print the path to the settings file for the character (or log it)
-            UE_LOG(LogTemp, Warning, TEXT("Found character settings: %s"), *SettingsFilePath);
+            if (bIsDirectory)
+            {
+                CharacterDirectories.Add(FilenameOrDirectory);
+            }
+            return true;  // Continue iteration
+        });
+
+    if (bFoundDirs)
+    {
+        for (FString CharacterDir : CharacterDirectories)
+        {
+            // Print debug message for each folder being checked
+            UE_LOG(LogTemp, Warning, TEXT("Checking folder: %s"), *CharacterDir);
+
+            FString SettingsFilePath = CharacterDir + TEXT("/settings/settings.txt");
+
+            // Check if the settings file exists
+            UE_LOG(LogTemp, Warning, TEXT("Checking for file: %s"), *SettingsFilePath);
+            if (FPaths::FileExists(SettingsFilePath))
+            {
+                // Print the path to the settings file for the character
+                UE_LOG(LogTemp, Warning, TEXT("Found character settings: %s"), *SettingsFilePath);
+            }
         }
     }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("No directories found in: %s"), *CharacterFolderPath);
+    }
 }
+
