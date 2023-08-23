a6e100030a7bb5bfcbb7ebe691361f3030d49883
chillionaire128 <33501553+chillionaire128@users.noreply.github.com>
2023-08-23 14:55:50 -0700

    loading settings.json in python
    Very basic skeleton for loading character settings.json in python

--- Content/Python/CharacterInfo.py
+++ Content/Python/CharacterInfo.py
@@ -1,3 +1,9 @@
+import json
 class CharacterInfo:
     def __init__(self, settings_file_path):
-        self.settings_file_path = settings_file_path
+        with open(settings_file_path, 'r') as file:
+            data = json.load(file)
+            self.name = data['name']
+            self.health = data['health']
+            self.selectIcon = data['selectIcon']
+            self.filePath = settings_file_path
---
+++ Content/Python/CharacterLoadingUtils.py
@@ -0,0 +1,27 @@
+import os
+from CharacterInfo import CharacterInfo
+
+def find_settingsfiles(start_dir):
+    """
+    Walks through character directory and finds all files called 'settings.json'
+    :param start_dir:
+    :return:
+    """
+    settings_file_paths = []
+    for root, dirs, files in os.walk(start_dir):
+        if 'settings.json' in files:
+            settings_file_paths.append(os.path.join(root, 'settings.json'))
+
+    return settings_file_paths
+
+def get_all_characters_info(character_directory):
+    """
+    Checks for settings files inside character directory and
+    initializes a CharacterInfo object based on info in the JSON file
+    :param character_directory: Path to the base directory containing the character folders
+    :return: List of CharacterInfo
+    """
+    characters_to_load = []
+    for settings_file in find_settingsfiles(character_directory):
+        characters_to_load.append(CharacterInfo(settings_file))
+    return characters_to_load
